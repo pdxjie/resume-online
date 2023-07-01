df9930b1a69124629d489c5c2d281494f153a408
gaochenjie <gaochenjie@lejingling.cn>
2023-07-01 11:18:07 +0800

    feat: 实现hover时给简历模板添加遮罩层样式

--- src/components/ResumeTemplateDraw.vue
+++ src/components/ResumeTemplateDraw.vue
@@ -12,20 +12,31 @@ export default defineComponent({
   },
   setup () {
     const placement = ref('right')
+    let isChecked = ref(false)
     const store = useStore()
-    const onClose = () => {
-      store.commit('CHANGE_TEMPLATE_STATUS')
+
+    const changeTemplate = (template) => {
+      isChecked.value = template.img === selectedResumeTemplate.value.img
     }
     const drawVisible = computed(() => {
       return store.state.resumeTemplateDrawVisible
     })
-    watch(drawVisible, (nVal, oVal) => {
-      console.log(nVal, 'val...')
+    const selectedResumeTemplate = computed(() => {
+      return store.state.selectedResumeTemplate
+    })
+    watch(selectedResumeTemplate, (nVal, oVal) => {
+      console.log(nVal, 'resume template....')
     }, { immediate: true, deep: true })
+    const onClose = () => {
+      store.commit('CHANGE_TEMPLATE_STATUS')
+    }
     return {
+      selectedResumeTemplate,
       placement,
       drawVisible,
-      onClose
+      isChecked,
+      onClose,
+      changeTemplate
     }
   }
 })
@@ -45,7 +56,31 @@ export default defineComponent({
       <a-button type="primary" @click="onClose">确定</a-button>
     </template>
     <div class="template-resume">
-      <img class="template-hover" v-for="template in templates" :key="template.img" :src="template.img" alt="">
+      <div class="template-selection" v-for="template in templates" :key="template.img">
+        <img class="template-hover" :src="template.img" alt="">
+        <div class="template-operation">
+          <a-tooltip placement="top">
+            <template #title>
+              <span>简历预览</span>
+            </template>
+            <a-button shape="circle">
+              <template #icon>
+                <eye-outlined />
+              </template>
+            </a-button>
+          </a-tooltip>
+          <a-tooltip placement="top">
+            <template #title>
+              <span>选择模板</span>
+            </template>
+            <a-button shape="circle">
+              <template #icon>
+                <select-outlined />
+              </template>
+            </a-button>
+          </a-tooltip>
+        </div>
+      </div>
     </div>
   </a-drawer>
 </template>
@@ -56,17 +91,46 @@ export default defineComponent({
   display: flex;
   justify-content: space-evenly;
   flex-wrap: wrap;
-  .template-hover {
-    object-fit: contain;
+  .template-selection {
+    position: relative;
     width: 33.3%;
     height: 100%;
     margin: 10px 30px;
     box-shadow: 0px 2px 2px 1px rgba(0, 0, 0, 0.2);
     border-radius: 8px;
-    padding: 20px 0px;
+
+    .template-hover {
+      width: 100%;
+      height: 100%;
+      padding: 18px 25px;
+    }
+    .template-operation {
+      position: absolute;
+      top: 40%;
+      left: 50%;
+      transform: translate(-50%);
+      opacity: 0;
+    }
   }
-  .template-hover:hover {
-    background: rgba(0, 0, 0, .3);
+  .template-selection:hover {
+    .template-operation {
+      opacity: 1;
+      display: flex;
+      ::v-deep .ant-btn {
+        margin: 0 10px;
+      }
+    }
+  }
+  .template-selection:hover.template-selection::before  {
+    content: " ";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 8px;
+    /*最后一个参数是半透明度，可以透过调整0-1的数值，调整到满意的透明度*/
+    background-color: rgba(0, 0, 0, 0.5);
   }
 }
 </style>
